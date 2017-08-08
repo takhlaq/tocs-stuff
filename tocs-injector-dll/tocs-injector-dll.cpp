45fa4b83fb4c63d05aee219e8b0ddfc44e33cadb
Tahir Akhlaq <xdemolish@hotmail.com>
2017-08-08 19:51:11 +0100

    added save prompt

---
+++ tocs-injector-dll/tocs-injector-dll.cpp
@@ -0,0 +1,135 @@
+// tocs-audio-toggle.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <fstream>
+#include <regex>
+
+#include "../minhook/MinHook.h"
+
+#pragma comment(lib, "kernel32.lib")
+
+HANDLE(WINAPI* pCreateFile)
+(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
+   DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) = CreateFileA;
+
+HANDLE WINAPI createFileDetour(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
+   DWORD dwFlagsAndAttributes, HANDLE hTemplateFile);
+
+BOOL CALLBACK enumWindows(__in  HWND hWnd, __in  LPARAM lParam);
+void replaceStr(std::string& in, const std::string& find, const std::string& replace);
+bool saveSDSlot = false;
+std::regex re("(?:.*|)english(?:\=|.*)([01])");
+HWND tocsWindow = nullptr;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+   switch (dwReason)
+   {
+   case DLL_PROCESS_ATTACH:
+      if (MH_Initialize() != MH_OK)
+         return false;
+      MH_CreateHookApiEx(L"kernel32", "CreateFileA", &createFileDetour, reinterpret_cast<LPVOID*>(&pCreateFile), nullptr);
+      MH_EnableHook(MH_ALL_HOOKS);
+      break;
+   case DLL_THREAD_ATTACH:
+   case DLL_THREAD_DETACH:
+      break;
+   case DLL_PROCESS_DETACH:
+      MH_DisableHook(MH_ALL_HOOKS);
+      MH_Uninitialize();
+      break;
+   }
+   return true;
+}
+
+HANDLE WINAPI createFileDetour(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+{
+   std::string fileName(lpFileName);
+
+   // confirm dialog for saving
+   bool isSDSlot = false;
+   if (dwDesiredAccess & GENERIC_WRITE && fileName.find("save") != std::string::npos || (isSDSlot = (fileName.find("sdslot") != std::string::npos)))
+   {
+      if (!isSDSlot)
+      {
+         auto pos = fileName.find_last_of("/");
+         EnumWindows(enumWindows, NULL);
+         saveSDSlot = MessageBoxA(tocsWindow, std::string("are you sure you wish to overwrite " + fileName.substr(pos, fileName.size() - pos)).c_str(), "Trails of Cold Steel Save Prompt", MB_YESNO) == IDYES;
+
+         if (!saveSDSlot)
+            return nullptr;
+      }
+      else if (saveSDSlot)
+      {
+         saveSDSlot = false;
+      }
+   }
+   else if (fileName.find("voice/wav") != std::string::npos)
+   {
+      std::ifstream conf("audioHookConf.ini");
+      bool enabled = false;
+      if (conf.good())
+      {
+         std::string line;
+         std::smatch match;
+
+         while (std::getline(conf, line))
+         {
+            if (line[0] == '#')
+               continue;
+
+            if (std::regex_search(line, match, re))
+            {
+               enabled = std::atoi(match[1].str().c_str());
+               
+               if (enabled)
+                  replaceStr(fileName, "/wav_jp/", "/wav/");
+
+               break;
+            }
+         }
+      }
+      else
+      {
+         std::ofstream("audioHookConf.ini").write("english = 0", sizeof("english = 0"));
+      }
+      conf.close();
+   }
+   return pCreateFile(fileName.c_str(), dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
+void replaceStr(std::string& in, const std::string& find, const std::string& replace)
+{
+   auto pos = 0;
+   while ((pos = in.find(find, pos)) != std::string::npos)
+   {
+      in.replace(pos, find.length(), replace);
+      pos += replace.length();
+   }
+}
+
+BOOL CALLBACK enumWindows(
+   __in  HWND hWnd,
+   __in  LPARAM lParam
+) {
+   if (!::IsIconic(hWnd)) {
+      return TRUE;
+   }
+
+   int length = ::GetWindowTextLengthA(hWnd);
+   if (0 == length) return TRUE;
+
+   std::string title;
+   title.reserve(length);
+
+   GetWindowTextA(hWnd, &title[0], title.size());
+
+   if (_strnicmp(title.c_str(), "The Legend of Heroes - Trails of Cold Steel", sizeof("The Legend of Heroes - Trails of Cold Steel") == 0))
+   {
+      tocsWindow = hWnd;
+      return FALSE;
+   }
+   return true;
+}
--- tocs-injector/injector.cpp
+++ tocs-injector/injector.cpp
@@ -18,15 +18,14 @@ HANDLE hProcess = nullptr;
 LPVOID LLParam = nullptr;
 PROCESSENTRY32 pe32;
 std::string dllPath;
-
+bool unhook = false;
 void cleanup();
+void injectDll(const std::string& name);
 
 int main()
 {
    std::cout << "[info] starting..." << std::endl;
 
-   // unhook on cleanup
-   std::atexit(cleanup);
    std::string enableStr("english = 0");
    {
       std::ifstream conf("audioHookConf.ini");
@@ -50,49 +49,26 @@ int main()
       conf.close();
    }
 
+   // unhook on cleanup
+   std::atexit(cleanup);
+   std::cout << "[info] injecting dll... " << "\\\\tocs-injector-dll.dll" << std::endl;
+
+find:
+   injectDll("\\\\tocs-injector-dll.dll");
    while (true)
-   {
+   {     
+      if (!hProcess && !unhook)
+      {
+         std::cout << "[error] unable to find Trails of Cold Steel process, checking again in 10 seconds..." << std::endl;
+         std::this_thread::sleep_for(std::chrono::seconds(10));
+         goto find;
+      }
       if (enabled)
       {
-         pe32.dwSize = sizeof(PROCESSENTRY32);
+         std::cout << "[success] found Trails of Cold Steel process" << std::endl;
+         std::cout << "[info] defaulting to EN audio - change areas to clear voice cache" << std::endl;
 
-         find:
-         HANDLE hTool32 = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
-         bProcess = Process32First(hTool32, &pe32);
-         while (bProcess && Process32Next(hTool32, &pe32))
-         {
-            if (pe32.szExeFile == std::wstring(L"ed8.exe") || pe32.szExeFile == std::wstring(L"ed8_jp.exe"))
-            {
-               std::string currentDir;
-               currentDir.reserve(MAX_PATH);
-
-               GetCurrentDirectoryA(MAX_PATH, &currentDir[0]);
-               dllPath = (&currentDir[0] + std::string("\\\\tocs-audio-toggle.dll"));
-
-               hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION |
-                  PROCESS_VM_WRITE, FALSE, pe32.th32ProcessID);
-
-               LPVOID LoadLibraryAddr = (LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
-
-               LLParam = (LPVOID)VirtualAllocEx(hProcess, NULL, dllPath.size(),
-                  MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-
-               WriteProcessMemory(hProcess, LLParam, dllPath.c_str(), dllPath.size(), NULL);
-               CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryAddr,
-                  LLParam, NULL, NULL);
-               std::cout << "[success] found Trails of Cold Steel process" << std::endl;
-               std::cout << "[info] defaulting to EN audio - change areas to clear voice cache" << std::endl;
-
-               std::ofstream("audioHookConf.ini").write("english = 1", enableStr.size());
-               break;
-            }
-         }
-         if (!hProcess)
-         {
-            std::cout << "[error] unable to find Trails of Cold Steel process, checking again in 15 seconds..." << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(15));
-            goto find;
-         }
+         std::ofstream("audioHookConf.ini").write("english = 1", enableStr.size());
       }
       else
       {
@@ -100,7 +76,6 @@ int main()
          bProcess = false;
          if (hProcess)
          {
-            cleanup();
             std::ofstream("audioHookConf.ini").write("english = 0", enableStr.size());
          }
       }
@@ -111,8 +86,6 @@ int main()
       {
          if (line == "qqq")
             exit(0);
-         else if (line == "help")
-            std::cout << "[info] unknown command, try <wat> instead" << std::endl;
          else if (line == "wat")
             std::cout << "[info] place jp voice files in Trails of Cold Steel/data/voice/wav_jp/ > hit enter in this window to toggle english/jp voices > change areas to clear voice cache"
             << std::endl << "[info] qqq to unhook and exit"
@@ -120,9 +93,53 @@ int main()
             << std::endl;
          else if (line.size() == 0 || line == "t")
             enabled = !enabled;
+         else if (line == "unhook")
+         {
+            unhook = !unhook;
+            enabled = false;
+            cleanup();
+         }
+         else
+            std::cout << "[info] unknown command, try <wat> instead" << std::endl;
       }
    }
    return 0;
+}
+
+void injectDll(const std::string& name)
+{
+   pe32.dwSize = sizeof(PROCESSENTRY32);
+   HANDLE hTool32 = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+   bProcess = Process32First(hTool32, &pe32);
+   bool found = false;
+
+   while (bProcess && Process32Next(hTool32, &pe32))
+   {
+      if (pe32.szExeFile == std::wstring(L"ed8.exe") || pe32.szExeFile == std::wstring(L"ed8_jp.exe"))
+      {
+         std::string currentDir;
+         currentDir.reserve(MAX_PATH);
+
+         GetCurrentDirectoryA(MAX_PATH, &currentDir[0]);
+         dllPath = (&currentDir[0] + name);
+
+         hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION |
+            PROCESS_VM_WRITE, FALSE, pe32.th32ProcessID);
+
+         LPVOID LoadLibraryAddr = (LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+
+         LLParam = (LPVOID)VirtualAllocEx(hProcess, NULL, dllPath.size(),
+            MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+         WriteProcessMemory(hProcess, LLParam, dllPath.c_str(), dllPath.size(), NULL);
+         CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryAddr,
+            LLParam, NULL, NULL);
+         found = true;
+         break;
+      }
+   }
+   if (!found)
+      hProcess = nullptr;
 }
 
 void cleanup()
@@ -143,7 +160,7 @@ void cleanup()
          GetModuleFileNameExA(hProcess, hMods[i], szPath, MAX_PATH);
          PathStripPathA(szPath);
 
-         if (!_stricmp(szPath, "tocs-audio-toggle.dll"))
+         if (!_stricmp(szPath, "tocs-injector-dll.dll"))
          {
             hMod = hMods[i];
          }
@@ -158,5 +175,7 @@ void cleanup()
 
       hProcess = nullptr;
       pe32 = PROCESSENTRY32();
+      pe32.dwSize = sizeof(PROCESSENTRY32);
+      std::cout << "[info] removed hook " << std::endl;
    }
 }
