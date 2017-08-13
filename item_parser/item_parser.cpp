ea1af986d32d605d0878a7c2cfa8d1eb0102dbcd
Tahir Akhlaq <xdemolish@hotmail.com>
2017-08-13 23:35:58 +0100

    fixed item parsing gud

--- item_parser/item_parser.cpp
+++ item_parser/item_parser.cpp
@@ -7,22 +7,33 @@
 #include <iomanip>
 #include <regex>
 
-struct item_entry
+struct TableHeader
 {
-   char item[4];
-   char padding;
-   char size;
+   std::string type;
+   uint16_t dataSize;
+};
+
+struct ItemHeader
+{
+   std::string dataType;
+   uint16_t unknown[23];
+};
+
+struct ItemEntry
+{
+   TableHeader entryHeader;
    uint16_t itemid;
-   char wat[56];
+   uint16_t padding;
+   ItemHeader itemHeader;
    std::string name;
    std::string description;
 };
 
-struct table_struct
+struct ItemTable
 {
    uint16_t count;
-   std::vector<item_entry> entries;
-};
+   std::vector<ItemEntry> entries;
+} data;
 
 // https://stackoverflow.com/a/3824338
 template <class T>
@@ -72,63 +83,52 @@ int main(int argc, char* argv[])
                   continue;
 
                auto offset = 0;
-               item_entry entry;
-               skipBytes = 6;
+               ItemEntry entry;
+               memset(&entry, 0, sizeof(entry));
                
-               memcpy(&entry.item[0], buf.data() + i, 4);
-               offset = 5;
-               skipBytes += *reinterpret_cast<uint8_t*>(buf.data() + i + offset);
-               offset += 2;
-               //endswap(reinterpret_cast<uint16_t*>(buf.data() + i + offset));
-               entry.itemid = *reinterpret_cast<uint16_t*>(buf.data() + i + offset);
-               offset += 2;
-               memcpy(&entry.wat[0], buf.data() + i + offset, sizeof(entry.wat));
-               offset += 56;
-
-               uint8_t ignoreJump = 0;
-               // this is incredibly retarded but seems to work so whatever
+               // table entry header
                {
-                  //if (static_cast<uint8_t>(entry.wat[0]) != 0xFD)
-                  {
-                     if (static_cast<uint8_t>(entry.wat[1]) != 0xFF)
-                        ignoreJump = offset = (offset - 56) + static_cast<uint8_t>(entry.wat[2]) + 1;
-                  }
+                  entry.entryHeader.type = std::string(buf.data() + i + offset);
+                  offset += entry.entryHeader.type.size() + 1;
+                  // number of bytes til end of data (also copies in item id and padding)
+                  memcpy(&entry.entryHeader.dataSize, buf.data() + i + offset, 2);
+                  offset += 2;
                }
-
-               while (*reinterpret_cast<uint8_t*>(buf.data() + i + offset) == 0xFF)
-                  offset++;
-               while (*reinterpret_cast<uint8_t*>(buf.data() + i + offset - 1) != 0xFF)
-                  offset--;
-
-
-               // this is incredibly retarded but seems to work so whatever
-               {
-                  entry.name = std::string(buf.data() + i + offset);
-
-                  while (entry.name.size() < 4 || entry.name == "item" || (!std::regex_match(entry.name.substr(0, 1), alphaNumeric) && ignoreJump < skipBytes))
-                     entry.name = std::string(buf.data() + i + (offset = ignoreJump++));
-               }
-               offset += entry.name.size() + 1;
-               entry.description = std::string(buf.data() + i + offset);
-               entry.description = entry.description.size() > 4 ? entry.description : "";
-               offset += entry.description.size();
-               
-               for (auto it = entry.description.begin(); it != entry.description.end(); ++it)
-                  if (*it == '\r' || *it == '\n')
-                     *it = ' ';
-                  else if (*it == ',')
-                     *it = '-';
-
-               replaceStr(entry.description, ",", "--");
-               replaceStr(entry.name, ",", "--");
 
                std::stringstream ss;
-               ss << "0x" << std::setw(4) << std::setfill('0') << std::uppercase << std::hex << entry.itemid;
+               // itemid
+               {
+                  memcpy(&entry.itemid, buf.data() + i + offset, 2);
+                  offset += 2;
+                  ss << "0x" << std::setw(4) << std::setfill('0') << std::uppercase << std::hex << entry.itemid;
+               }
+               // padding
+               {
+                  memcpy(&entry.padding, buf.data() + i + offset, 2);
+                  offset += 2;
+               }
+               // item header
+               {
+                  entry.itemHeader.dataType = std::string(buf.data() + i + offset);
+                  offset += entry.itemHeader.dataType.size() + 1;
+                  memcpy(&entry.itemHeader.unknown[0], buf.data() + i + offset, sizeof(entry.itemHeader.unknown));
+                  offset += sizeof(entry.itemHeader.unknown);
+               }
 
-               std::string outStr(ss.str() + ", " + entry.name + ", " + entry.description + "\r\n");
-               //std::cout << "filePos " << std::to_string(i) << " skipBytes " << std::to_string(skipBytes) << " offset " << std::to_string(offset) << std::endl;
-               //std::cout << outStr << std::endl;
-               outCsv.write( outStr.c_str(), outStr.size());
+               entry.name = std::string(buf.data() + i + offset);
+               entry.description = std::string(buf.data() + i + offset + entry.name.size() + 1);
+               for (auto j = 0; j < entry.description.size(); ++j)
+                  if (entry.description[j] == '\r' || entry.description[j] == '\n')
+                     entry.description[j] = ' ';
+                  else if (entry.description[j] == ',')
+                     entry.description[j] = '-';
+
+               replaceStr(entry.name, ",", "-");
+               replaceStr(entry.description, ",", "-");
+
+               std::string outStr(ss.str() + ", " + entry.name + ", " + entry.description + "\n");
+               outCsv.write(outStr.c_str(), outStr.size());
+               skipBytes = entry.entryHeader.dataSize + entry.entryHeader.type.size() + 2;
             }
          }
       }
