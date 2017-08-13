069260e12285d0b96d4b64ba4df8d70aef12f3f3
Tahir Akhlaq <xdemolish@hotmail.com>
2017-08-13 04:15:53 +0100

    EVERYBODY JUMP

--- item_parser/item_parser.cpp
+++ item_parser/item_parser.cpp
@@ -22,26 +22,6 @@ struct table_struct
    std::vector<item_entry> entries;
 };
 
-
-template <typename T>
-T swap_endian(T u)
-{
-   static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
-
-   union
-   {
-      T u;
-      unsigned char u8[sizeof(T)];
-   } source, dest;
-
-   source.u = u;
-
-   for (size_t k = 0; k < sizeof(T); k++)
-      dest.u8[k] = source.u8[sizeof(T) - k - 1];
-
-   return dest.u;
-}
-
 void replaceStr(std::string& in, const std::string& find, const std::string& replace)
 {
    auto pos = 0;
@@ -71,8 +51,6 @@ int main(int argc, char* argv[])
          {
             table.close();
             auto skipBytes = 0;
-            table_struct tableStruct;
-            tableStruct.count = *reinterpret_cast<uint16_t*>(buf.data() + 0);
 
             std::ofstream outCsv("items.csv");
             outCsv.clear();
@@ -90,27 +68,37 @@ int main(int argc, char* argv[])
                memcpy(&entry.item[0], buf.data() + i, 4);
                offset = 5;
                skipBytes += *reinterpret_cast<uint8_t*>(buf.data() + i + offset);
-               offset += 1;
-               entry.itemid = swap_endian(*reinterpret_cast<uint16_t*>(buf.data() + i + offset));
+               offset += 2;
+               entry.itemid = *reinterpret_cast<uint16_t*>(buf.data() + i + offset);
                offset += 2;
                memcpy(&entry.wat[0], buf.data() + i + offset, sizeof(entry.wat));
                offset += 56;
+
+               while (*reinterpret_cast<uint8_t*>(buf.data() + i + offset) == 0xFF)
+                  offset++;
+               while (*reinterpret_cast<uint8_t*>(buf.data() + i + offset - 1) != 0xFF)
+                  offset--;
+
                entry.name = std::string(buf.data() + i + offset);
                offset += entry.name.size() + 1;
                entry.description = std::string(buf.data() + i + offset);
                offset += entry.description.size();
-               replaceStr(entry.description, ",", ";");
-               replaceStr(entry.name, ",", ";");
+               
+               for (auto it = entry.description.begin(); it != entry.description.end(); ++it)
+                  if (*it == '\r' || *it == '\n')
+                     *it = ' ';
+                  else if (*it == ',')
+                     *it = '-';
+
+               replaceStr(entry.description, ",", "--");
+               replaceStr(entry.name, ",", "--");
 
                std::string outStr(std::to_string(entry.itemid) + ", " + entry.name + ", " + entry.description + "\r\n");
                std::cout << "filePos " << std::to_string(i) << " skipBytes " << std::to_string(skipBytes) << " offset " << std::to_string(offset) << std::endl;
                std::cout << outStr << std::endl;
                outCsv.write( outStr.c_str(), outStr.size());
-               //tableStruct.entries.push_back(entry);
-               //skipBytes = offset + 0;
             }
          }
-         std::cin;
       }
    }
    else
